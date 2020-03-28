33b2750dd279df7b806321a8e49f9b6f469bdcb0
zhongkun <1064598949@qq.com>
2020-03-28 10:36:14 +0800

    引入element-ui

--- src/components/page/pos.vue
+++ src/components/page/pos.vue
@@ -1,13 +1,35 @@
 <template>
-   <div class="pos"></div>
+   <div class="pos">
+       <el-row>
+          <el-col :span="7" class="pos-order" id="order-list">
+             我的订单
+          </el-col>
+           <div>
+              11
+               <el-col>
+             我是产品11
+          </el-col>
+           </div>
+       </el-row>
+   </div>
 </template>
 
 <script>
 export default {
-
+  mounted(){
+    let orderHeight = document.body.clientHeight
+    document.getElementById('order-list').style.height = orderHeight+'px'
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.pos {
+   color: #666;
+}
+.pos-order {
+     background: #fff;
+     border-right: 1px solid #666;
+     
+ }
 </style>
